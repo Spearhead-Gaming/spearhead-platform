bff0799a8be3101e3f01a37c0a5d99fa43763947
Majestic44 <majestic44@users.noreply.github.com>
2026-08-07 18:05:07 -0400

    Update of test and lint

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/build/**",
+      "**/coverage/**",
+      "**/.turbo/**",
+      "**/.next/**"
+    ]
+  },
+
+  eslint.configs.recommended,
+
+  ...tseslint.configs.recommended,
+
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_"
+        }
+      ],
+
+      "@typescript-eslint/no-explicit-any": "warn"
+    }
+  }
+);
